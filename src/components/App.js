268654c9348a5c770014ece175c13dc976757e01
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-29 22:55:30 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
-import Todo from "./ToDo";
-
+import Todo from "./Todo";
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <Todo />
+      <Todo />
+
     </div>
   )
 }
